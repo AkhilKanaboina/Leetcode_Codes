e6d114687c966e05f5d9e4b59b24c68b59d3d146
Kanaboina Akhil <126093461+AkhilKanaboina@users.noreply.github.com>
2025-07-06 14:38:03 +0530

    Time: 2 ms (17.9%), Space: 58.4 MB (30.68%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.java
@@ -1,25 +1,23 @@
 class Solution {
     public int minSubArrayLen(int target, int[] nums) {
-        int n=nums.length,ans=Integer.MAX_VALUE;
-        int pref[]=new int[n];
-        pref[0]=nums[0];
-        for(int i=1;i<n;i++){
-            pref[i]+=(pref[i-1]+nums[i]);
-        }
-        HashMap<Integer,Integer> mp=new HashMap<>();
-        for(int i=0;i<n;i++){
-            int temp=pref[i]%target;
-            if(!mp.containsKey(temp)){
-                mp.put(temp,i);
+        int i = 0, j = 0, n = nums.length;
+        int sum = nums[j];
+        int res, ans = Integer.MAX_VALUE;
+        while(j<n&&i<=j){
+            if(sum>=target){
+                ans=Math.min(ans,(j-i+1));
+            }
+            
+            if(sum<target&& j<n-1){
+                j++;
+                sum+=nums[j];
+
             }else{
-                ans=Math.min(ans,(i-mp.get(temp)));
-                mp.put(temp,i);
+                sum-=nums[i];
+                i++;
             }
         }
-        if(ans==Integer.MAX_VALUE){
-            return 0;
-        }
-        return ans;
-    
+
+        return ans == Integer.MAX_VALUE ? 0 : ans;
     }
 }
