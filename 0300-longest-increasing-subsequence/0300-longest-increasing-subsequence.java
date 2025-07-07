5a8c559c8d8c8852bdaa3e21ee5d0f2d87b43cbd
Kanaboina Akhil <126093461+AkhilKanaboina@users.noreply.github.com>
2025-07-07 14:49:51 +0530

    Time: 6 ms (88.31%), Space: 44.1 MB (87.06%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,20 +1,58 @@
 class Solution {
-    public int lengthOfLIS(int[] nums) {
-        int n=nums.length;
-        int a[]=new int[n];
-        int max=1;
-        for(int i=0;i<n;i++){
-            a[i]=1;
-        }
-        for(int i=0;i<n;i++){
-               // a[i]=1;
-            for(int j=0;j<i;j++){
-                if(nums[j]<nums[i]){
-                    a[i]=Math.max(a[i],a[j]+1);
-                }
+    public int binarySearch(List<Integer> list,int target){
+        int l=0;
+        int r=list.size()-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(list.get(mid)==target){
+                return mid;
+            }else if(list.get(mid)>target){
+                r=mid-1;
+            }else{
+                l=mid+1;
             }
-            max=Math.max(max,a[i]);
         }
-        return max;
+        return l;
+    }
+    public int lengthOfLIS(int[] nums) {
+       ArrayList<Integer> list=new ArrayList<>();
+       for(int n:nums){
+        if(list.isEmpty()||list.get(list.size()-1)<n){
+            list.add(n);
+        }else{
+            int indx=binarySearch(list,n);
+            list.set(indx,n);
+        }
+       }
+       return list.size();
     }
 }
+
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         int n=nums.length;
+//         int dp[]=new int[n];   
+//         int max=0;
+//         for(int i=0;i<n;i++){
+//             dp[i]=1;
+//         }
+//         Queue<Integer> min=new PriorityQueue<>();
+//         min.add(nums[0]);
+//         int j=0;
+//         for(int i=1;i<n;i++){
+//                 while(!min.isEmpty()&&min.peek()<nums[i]&&j<i){
+//                     dp[i]=Math.max(dp[i],dp[j]+1);
+//                     min.poll();
+//                     j++;
+//                 }
+//             min.add(nums[i]);
+//         }
+//         for(int i=0;i<n;i++){
+//             if(dp[i]>max){
+//                 max=dp[i];
+//             }
+//         }
+//         return max;
+//     }
+// }
+
