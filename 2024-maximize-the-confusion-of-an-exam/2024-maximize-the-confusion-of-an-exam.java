01a5cedd4db73f9e9403ac61e1ec6adc12987953
Kanaboina Akhil <126093461+AkhilKanaboina@users.noreply.github.com>
2025-10-30 11:55:37 +0530

    Time: 13 ms (59.5%), Space: 45.1 MB (22.05%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.java
@@ -0,0 +1,25 @@
+class Solution {
+    String s;
+    int max=0;
+    public int maxConsecutiveAnswers(String answerKey, int k) {
+        s=answerKey;
+        max=k;
+        return Math.max(slideWindow('T'),slideWindow('F'));
+    }
+    public int slideWindow(char target){
+        int count=0;
+        int left=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==target){
+                count++;
+            }
+            if(count>max){
+                if(s.charAt(left)==target){
+                    count--;
+                }
+                left++;
+            }
+        }
+        return s.length()-left;
+    }
+}
