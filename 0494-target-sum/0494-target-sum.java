dc64bc298737993c52830bdae7c29deb34a5e8e8
Kanaboina Akhil <126093461+AkhilKanaboina@users.noreply.github.com>
2025-11-13 20:58:40 +0530

    Time: 156 ms (34.12%), Space: 47.8 MB (5.45%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,6 +1,7 @@
 class Solution {
     int target;
     int size;
+    Map<String,Integer> memo=new HashMap<>();
     public int findTargetSumWays(int[] nums, int val) {
         target=val;
         size=nums.length;
@@ -10,8 +11,12 @@ class Solution {
         if (n == size) {
              return (val == target) ? 1 : 0;
         }
+        if(memo.containsKey(n+","+val)){
+            return memo.get(n+","+val);
+        }
         int neg=dp(nums,n+1,val-nums[n]);
         int pos=dp(nums,n+1,val+nums[n]);
+        memo.put(n+","+val,neg+pos);
         return neg+pos;
     }
 }
