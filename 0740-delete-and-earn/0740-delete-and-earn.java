7ef0eabfb070d52affede04992cce19f948c280b
Kanaboina Akhil <126093461+AkhilKanaboina@users.noreply.github.com>
2025-11-21 22:58:37 +0530

    Time: 5 ms (57.77%), Space: 46.3 MB (30.39%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int deleteAndEarn(int[] nums) {
+        int sumTable[]=new int[10010];
+        Arrays.fill(sumTable,0);
+        int max=Integer.MIN_VALUE;
+        for(int num:nums){
+            sumTable[num]+=num;
+            max=Math.max(max,num);
+        }
+        int dp[]=new int[max+1];
+        dp[0]=sumTable[0];
+        dp[1]=sumTable[1];
+        int ans=dp[1];
+        for(int i=2;i<=max;i++){
+            dp[i]=Math.max(dp[i-1],dp[i-2]+sumTable[i]);
+            ans=Math.max(ans,dp[i]);
+        }
+        return ans;
+    }
+}
