d95263f0eed1d6ef20deb4ef379258678661707e
Kanaboina Akhil <126093461+AkhilKanaboina@users.noreply.github.com>
2025-11-27 21:45:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n=nums.length;
+        int arr[]=new int[n];
+        Arrays.fill(arr,-1);
+        Stack<Integer> stack=new Stack<>();
+        Stack<Integer> index=new Stack<>();
+        boolean once=true;
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && stack.peek()<nums[i]){
+                arr[index.pop()]=nums[i];
+                stack.pop();
+            }
+            stack.push(nums[i]);
+            index.push(i);
+            if( i==n-1 && once && !stack.isEmpty()){
+                i=0;
+                once=false;
+            }
+        }
+        return arr;
+    }
+}
