d81e7d9fb00591cc3be8764492f73a4723b335ef
Kanaboina Akhil <126093461+AkhilKanaboina@users.noreply.github.com>
2025-11-29 22:23:49 +0530

    Time: 42 ms (31.55%), Space: 46.3 MB (24.05%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,16 +1,90 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
-        long ans=0;
+        int minLeft[]=leftToRight_Influnce_min(nums);
+        int minRight[]=righttoLeft_Influnce_min(nums);
+        
+        int maxLeft[]=leftToRight_Influnce_max(nums);
+        int maxRight[]=righttoLeft_Influnce_max(nums);
+        long min=sum(minLeft,minRight,nums);
+        long max=sum(maxLeft,maxRight,nums);
+        return max-min;
+    }
+    public long sum(int minLeft[],int minRight[],int nums[]){
         int n=nums.length;
+        long ans=0;
         for(int i=0;i<n;i++){
-            int min=nums[i];
-            int max=nums[i];
-            for(int j=i+1;j<n;j++){
-                min=Math.min(min,nums[j]);
-                max=Math.max(max,nums[j]);
-                ans=ans+(max-min);
-            }
+            ans=ans+(long)minLeft[i]*minRight[i]*nums[i];
         }
         return ans;
     }
+    public int[] leftToRight_Influnce_min(int nums[]){
+        Stack<Integer> stack=new Stack<>();
+        int n=nums.length;
+        int ans[]=new int[n];
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && nums[stack.peek()]>nums[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                ans[i]=i+1;
+            }else{
+                ans[i]=(i-stack.peek());
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+    public int[] righttoLeft_Influnce_min(int nums[]){
+        Stack<Integer> stack=new Stack<>();
+        int n=nums.length;
+        int ans[]=new int[n];
+        for(int i=n-1;i>=0;i--){
+            while(!stack.isEmpty() && nums[stack.peek()]>=nums[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                ans[i]=n-i;
+            }else{
+                ans[i]=(stack.peek()-i);
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+    public int[] leftToRight_Influnce_max(int nums[]){
+        Stack<Integer> stack=new Stack<>();
+        int n=nums.length;
+        int ans[]=new int[n];
+        for(int i=0;i<n;i++){
+            while(!stack.isEmpty() && nums[stack.peek()]<nums[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                ans[i]=i+1;
+            }else{
+                ans[i]=(i-stack.peek());
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+    public int[] righttoLeft_Influnce_max(int nums[]){
+        Stack<Integer> stack=new Stack<>();
+        int n=nums.length;
+        int ans[]=new int[n];
+        for(int i=n-1;i>=0;i--){
+            while(!stack.isEmpty() && nums[stack.peek()]<=nums[i]){
+                stack.pop();
+            }
+            if(stack.isEmpty()){
+                ans[i]=n-i;
+            }else{
+                ans[i]=(stack.peek()-i);
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+
+
 }
