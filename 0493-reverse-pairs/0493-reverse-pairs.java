5edb28b6684c8e78628f300936d80ef232646486
Kanaboina Akhil <126093461+AkhilKanaboina@users.noreply.github.com>
2025-12-25 17:38:15 +0530

    Time: 41 ms (89.65%), Space: 60.1 MB (54.14%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.java
+++ 0493-reverse-pairs/0493-reverse-pairs.java
@@ -1,50 +1,59 @@
 class Solution {
-    int nums[];
+
     int temp[];
 
     public int reversePairs(int[] nums) {
-        int n=nums.length;
-        this.nums=nums;
-        temp=new int[n];
-        return merge(0,n-1);
-        
+        temp=new int[nums.length];
+       return mergeSort(nums,0,nums.length-1);
     }
-    public int merge(int left,int right){
+    public int mergeSort(int nums[],int left,int right){
+        int ans=0;
         if(left>=right){
-            return 0;
+            return ans;
         }
         int mid=(left+right)/2;
-        int ans=(merge(left,mid) +  merge(mid+1,right));
+        ans+=mergeSort(nums,left,mid);
+        ans+=mergeSort(nums,mid+1,right);
+        ans+=count(nums,left,mid,right);
+        merge(nums,left,mid,right);
+        return ans;
+    }
+    public int count(int nums[],int left,int mid,int right){
+        int ans=0;
         int i=left;
         int j=mid+1;
-       
-        while(i<=mid&& j<=right){
-            if(nums[i]<=2*nums[j]){
+        while(i<=mid && j<=right){
+            if(nums[i]<=2L*nums[j]){
                 i++;
             }else{
-                j++;
                 ans+=(mid-i+1);
+                j++;
             }
-        }
-        i=left;
-        j=mid+1;
-        int key=0;
-        while(i<=mid && j<=right){
-            if(nums[i]<nums[j]){
-                temp[key++]=nums[i++];
-            }else{
-                temp[key++]=nums[j++];
-            }
-        }
-        while(i<=mid){
-            temp[key++]=nums[i++];
-        }
-        while(j<=right){
-            temp[key++]=nums[j++];
-        }
-        for(int a=left;a<=right;a++){
-            nums[a]=temp[a-left];
         }
         return ans;
     }
+    public void merge(int nums[],int left,int mid,int right){
+        int k=0;
+        int i=left;
+        int j=mid+1;
+        while(i<=mid && j<=right){
+            if(nums[i]<=nums[j]){
+                temp[k++]=nums[i++];
+            }else{
+                temp[k++]=nums[j++];
+            }
+        }
+        while(i<=mid){
+            temp[k++]=nums[i++];
+        }
+        while(j<=right){
+            temp[k++]=nums[j++];
+        }
+        k=0;
+        for(int a=left;a<=right;a++){
+            nums[a]=temp[k++];
+        }
+    }
+
+    
 }
