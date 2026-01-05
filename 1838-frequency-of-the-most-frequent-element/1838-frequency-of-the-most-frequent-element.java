e6f652a1f933e3195ef6128a3ce3eae7192aa31b
Kanaboina Akhil <126093461+AkhilKanaboina@users.noreply.github.com>
2026-01-05 18:56:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.java
@@ -1,21 +1,18 @@
 class Solution {
     public int maxFrequency(int[] nums, int k) {
         //brute force is to concider a "i" indexed number as max frequcy and checking how may number can reach it after sorting .. better one is using prefix not canculate sum every time using formula num[i]*(r-l+1)-prefix of[l...r]
+        int prefix=0;
         Arrays.sort(nums);
         int n=nums.length;
-        int ans=0;
-        long slideSum=0;
-        int cost=0;
         int left=0;
         int right=0;
-         for(right=0;right<n;right++){
-           slideSum+=nums[right];
-            while((long)(right-left+1)*nums[right]-slideSum>k && left<=right){
-                slideSum-=nums[left];
-                left++;
+        for(right=0;right<n;right++){
+            prefix+=nums[right];
+            int val=(right-left+1)*nums[right];
+            if(val-prefix>k){
+                prefix-=nums[left++];
             }
-            ans=Math.max(ans,right-left+1);
         }
-        return ans;
+        return n-left;
     }
 }
