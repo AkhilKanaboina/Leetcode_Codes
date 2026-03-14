a326ea8abd5a3a2c872dc2be9f316487eabee301
Kanaboina Akhil <126093461+AkhilKanaboina@users.noreply.github.com>
2026-03-14 11:40:59 +0530

    Time: 26 ms (13.23%), Space: 46.5 MB (43%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -3,12 +3,12 @@ class Solution {
     public String getHappyString(int n, int k) {
         list=new ArrayList<>();
         char c[]={'a','b','c'};
-        recursion(n,k,0,"",c);
+        recursion(n,0,"",c);
         if(list.size()<k) return "";
-        Collections.sort(list);
+       // Collections.sort(list);
         return list.get(k-1);
     }
-    public void recursion(int n,int k,int len,String s,char c[]){
+    public void recursion(int n,int len,String s,char c[]){
         if(len>n)return;
         if(len==n){
            list.add(s);
@@ -16,10 +16,10 @@ class Solution {
         }
         for(int i=0;i<c.length;i++){
             if(len>0 && s.charAt(len-1)!=c[i]){
-                recursion(n,k,len+1,s+c[i],c);
+                recursion(n,len+1,s+c[i],c);
             }
             if(len==0){
-                recursion(n,k,len+1,s+c[i],c);
+                recursion(n,len+1,s+c[i],c);
             }
         }
     }
