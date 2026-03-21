8cc54f6bdcc9645d98f572a16f6d581839a3b493
Kanaboina Akhil <126093461+AkhilKanaboina@users.noreply.github.com>
2026-03-21 13:58:38 +0530

    Time: 6 ms (97.11%), Space: 47.3 MB (97.55%) - LeetHub

--- 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -9,39 +9,16 @@
  */
 
 class Solution {
-    List<TreeNode> list=new ArrayList<>();
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        LCA(root,p);
-        List<TreeNode> dest=new ArrayList<>(list);
-        list.clear();
-        LCA(root,q);
-        TreeNode ans=null;
-        int key=0;
-        while(key<list.size() &&key< dest.size()){
-            if(list.get(key)==dest.get(key)){
-                ans=list.get(key);
+        while(root!=null){
+            if(root.val>p.val && root.val>q.val){
+                root=root.left;
+            }else if(root.val<p.val && root.val<q.val){
+                root=root.right;
             }else{
-                break;
+                return root;
             }
-            key++;
         }
-        return ans;
-        
-    }
-    public boolean LCA(TreeNode root,TreeNode temp){
-        if(root==null){
-            return false;
-        }
-        list.add(root);
-        if(root.val==temp.val){
-            return true;
-        }
-        if(root.val>temp.val){
-            if(LCA(root.left,temp)) return true;
-        }else{
-            if(LCA(root.right,temp)) return true;
-        }
-        list.remove(list.size()-1);
-        return false;
+        return null;
     }
 }
